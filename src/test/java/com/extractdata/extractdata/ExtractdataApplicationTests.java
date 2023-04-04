b4ed207cfb520745f1c4066d0903c5bba6eb8547
ashwinishivshette932 <ashwinishivshette932@gmail.com>
2023-04-04 16:56:26 +0530

    Updated test case to check sanity

--- src/test/java/com/extractdata/extractdata/ExtractdataApplicationTests.java
+++ src/test/java/com/extractdata/extractdata/ExtractdataApplicationTests.java
@@ -1,13 +1,21 @@
 package com.extractdata.extractdata;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.extractdata.extractdata.controller.ExtractDataController;
 
 @SpringBootTest
 class ExtractdataApplicationTests {
 
+	@Autowired
+	private ExtractDataController extractDataController;
+	
 	@Test
 	void contextLoads() {
+		Assertions.assertThat(extractDataController).isNotNull();
 	}
 
 }
